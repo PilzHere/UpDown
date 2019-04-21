ba3ad11554ed8ca6130034ba69c3774094392bf5
Unknown <svampodling@gmail.com>
2019-04-21 02:30:10 +0200

    Desktop config added.

--- desktop/src/mysko/pilzhere/updown/desktop/DesktopLauncher.java
+++ desktop/src/mysko/pilzhere/updown/desktop/DesktopLauncher.java
@@ -2,11 +2,27 @@ package mysko.pilzhere.updown.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+
 import mysko.pilzhere.updown.Updown;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "UpDown";
+		config.width = 854;
+		config.height = 480;
+		config.fullscreen = false;
+		config.resizable = true;
+//		config.addIcon(path, fileType);
+
+		config.initialBackgroundColor = Color.WHITE;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 60;
+		config.samples = 0;
+		config.vSyncEnabled = false;
+
 		new LwjglApplication(new Updown(), config);
 	}
 }
