919f8306551b3eafdb6cd700b0ec5207396090a0
Unknown <svampodling@gmail.com>
2019-04-21 02:08:48 +0200

    GDX added.

---
+++ desktop/src/mysko/pilzhere/updown/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mysko.pilzhere.updown.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mysko.pilzhere.updown.Updown;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Updown(), config);
+	}
+}
---
+++ ios/src/mysko/pilzhere/updown/IOSLauncher.java
@@ -0,0 +1,22 @@
+package mysko.pilzhere.updown;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import mysko.pilzhere.updown.Updown;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Updown(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
